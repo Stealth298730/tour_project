0477f2d920fba92781b312b74fafe4b32b9c9a9e
Stealth298730 <serhii.lundiak@gmail.com>
2024-12-03 15:28:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/tour')
+def tour():
+    return render_template('tour.html')
+
+
+@app.route('/departure')
+def departure():
+    return render_template('departure.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
